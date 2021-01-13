a32e66519ea87dda9518d9fe4002dbbc352ea735
Sai Bobby <saivkadiyala97@gmail.com>
2021-01-12 20:15:46 -0500

    added redis and tested

--- worker/fetch-github.js
+++ worker/fetch-github.js
@@ -1,5 +1,21 @@
 var fetch = require('node-fetch');
 
+const bluebird  = require('bluebird');
+
+//
+const redis = require('redis'); //redis is a in memory data store [not persistent as databases]
+const client = redis.createClient();
+//checking  if it connected or not
+client.on('connect', function() {
+  console.log('connected to redis succesfully!');
+  bluebird.promisifyAll(client);
+  console.log('promisified!')
+});
+
+const { promisify } = require("util");
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
 const baseURL = 'https://jobs.github.com/positions.json';
 
 var jobs;
@@ -29,7 +45,7 @@ async function oldFetchGithub(){
     
 
 }
-
+var totalComp = [];
 async function fetchGithub(){
     var onPage = 0;
     let resultCount =1;
@@ -41,9 +57,20 @@ async function fetchGithub(){
         allJobs.push(...jobs);
         console.log("Got Jobs: " , jobs.length);
         resultCount = jobs.length;
+        const companies = await jobs.map(job=>{
+            return {name: job.company , location: job.location};
+        });
+        //console.log(companies);
+        totalComp.push(companies);
+        console.log("Found companies no. :", companies.length);
     }
     
     console.log('Final allJobs : ' , allJobs.length);
+
+    const success = await setAsync('github',JSON.stringify(allJobs));
+    console.log({success});
+    const companiesAndLoc = await setAsync('companies',JSON.stringify(totalComp));
+    console.log({companiesAndLoc});
 }
 //fetchGithub();
 
